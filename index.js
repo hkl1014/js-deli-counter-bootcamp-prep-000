c965a4c3c7bfe5323b94b25fabfc515784043168
hkl1014 <linghangking93@hotmail.com>
2018-01-13 04:28:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(katzDeliLine){
     return "The line is currently empty."
   }
   for(let i = katzDeliLine.length;i<0;i--){
-    string+=`${katzDeliLine.length-i+1}. ${katzDeliLine[katzDeliLine.length-i]}`
+    string+=`${katzDeliLine.length-i+1}}`
     if(i!=katzDeliLine){string+=", ";}
   }
   return `The line is currently: ${string}`
