c9e02ecf5dec5df4aee4fd885f4eb8a8fd02aaae
hkl1014 <linghangking93@hotmail.com>
2018-01-13 04:01:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var katzDeli=[];
 
 function takeANumber(katzDeliLine,name){
   katzDeli.push(name);
-  let string = "Welcome, $(name). You are number $(katzDeli.length) in line "
+  let string = "Welcome "+ name +". You are number "+ katzDeli.length +" in line."
   return string
 }
