282b100ab69bef75b8e7e076593aba08c354e1ad
hkl1014 <linghangking93@hotmail.com>
2018-01-13 04:06:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function takeANumber(katzDeliLine,name){
   let string = "Welcome, "+ name +". You are number "+ katzDeliLine.length +" in line."
   return string
 }
+
+function nowServing(katzDeline){
+  if (katzDeliLine.length>0){
+    katzDeliLine.splice(0,1);
+  }
+  else{return "There is nobody waiting to be served"}
+}
