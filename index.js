6a3acb51ffee4ea165b01f108be783dad149636b
hkl1014 <linghangking93@hotmail.com>
2018-01-13 04:08:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ function takeANumber(katzDeliLine,name){
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length>0){
+    let a = katzDeliLine[0];
     katzDeliLine.splice(0,1);
+    return "Currently serving "+ a
   }
   else{return "There is nobody waiting to be served!"}
 }
