3bea19f2ff1ee0bd926e7bfd71623b721eaf992f
hkl1014 <linghangking93@hotmail.com>
2018-01-13 04:38:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function nowServing(katzDeliLine){
 
 function currentLine(katzDeliLine){
   var string1="";
-      
+
   if(katzDeliLine.length==0){
     return "The line is currently empty."
   }
